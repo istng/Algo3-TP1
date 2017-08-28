15d1a9e83f0c850ffc17e4bc9990ec73c0f69c14
istng <ivanstng@gmail.com>
2017-08-27 23:05:19 -0300

    ya casi esta para mandar archivos y debuguear

--- agentes.cpp
+++ agentes.cpp
@@ -1,19 +1,19 @@
 #include "auxiliares.h"
 
 
-bool puedoAgregarlo(std::vector< std::vector<int> > agentes, std::vector<int>& restantes, std::vector<int>& elegidos, int actual){
+bool puedoAgregarlo(std::vector< std::vector<int> > agentes, std::vector<int>& elegidos, int actual){
     bool res = true;
     for (int i = 0; i < elegidos.size(); ++i){
-        if (agentes[elegidos[i]][actual] + agentes[actual][elegidos[i]] == 0){res = false;}
+        if (agentes[elegidos[i]][actual] == -1 || agentes[actual][elegidos[i]] == -1){res = false;}
     }
     return res;
 }
 
-bool puedoAgregarloConPoda(std::vector< std::vector<int> > agentes, std::vector<int>& restantes, std::vector<int>& elegidos, int actual){
+bool puedoAgregarloConPoda(std::vector< std::vector<int> > agentes, std::vector<int>& elegidos, int actual){
     bool res = true;
     for (int i = 0; i < elegidos.size(); ++i){
         for (int j = 0; j < agentes.size(); ++j){
-            if(agentes[i][j] + agentes[actual][j] == 0) {res = false;}
+            if(agentes[i][j] == -1 || agentes[actual][j] == -1) {res = false;}
         }
     }
     return res;
@@ -56,15 +56,17 @@ void confiablesSinPodasAux(std::vector< std::vector<int> > agentes, std::vector<
 
             int actual = restantes[0];                                  //tomo el primero de los restantes restantes
             restantes.erase(restantes.begin());                         //lo quito, para no repetirlo en próximos llamados
+            imprimirVector(restantes);
 
-
-            if(puedoAgregarlo(agentes, restantes, elegidos, actual)){   //me fijo si puedo agregar o no al agente al conjunto actual
-                    elegidos.push_back(actual);                         //con el agente actual
-                    confiablesSinPodasAux(agentes, restantes, elegidos);
+            if(puedoAgregarlo(agentes, elegidos, actual)){   //me fijo si puedo agregar o no al agente al conjunto actual
+                elegidos.push_back(actual);                             //con el agente actual
+                std::cout << restantes.size();
+                confiablesSinPodasAux(agentes, restantes, elegidos);
             }
     
             if(habiaQueAgregarlo(agentes, elegidos, actual)){           //si algun elegido lo necesitaba, cortamos aca porque la instancia sería invalida
                 if(elegidos.size() != 0){elegidos.pop_back();}          //sin el agente actual
+                std::cout << restantes.size();
                 confiablesSinPodasAux(agentes, restantes, elegidos);
             }
 
@@ -87,7 +89,7 @@ void confiablesConPodasAux(std::vector< std::vector<int> > agentes, std::vector<
             restantes.erase(restantes.begin());                         //lo quito, para no repetirlo en próximos llamados
 
 
-            if(puedoAgregarloConPoda(agentes, restantes, elegidos, actual)){       //me fijo si puedo agregar o no al agente al conjunto actual
+            if(puedoAgregarloConPoda(agentes, elegidos, actual)){       //me fijo si puedo agregar o no al agente al conjunto actual
                 if(valeLaPena(agentes, restantes, elegidos, actual)){    //utilizo mis condiciones de poda de soluciones válidas
                     elegidos.push_back(actual);             			//con el agente actual
                     confiablesConPodasAux(agentes, restantes, elegidos);
@@ -107,58 +109,59 @@ void confiablesConPodasAux(std::vector< std::vector<int> > agentes, std::vector<
 
 int main(int argc, char** argv){
 
-    if (!(argc == 2 || argc == 1)){
+    if (!(argc == 3 || argc == 2 || argc == 1)){
         std::cout << std::endl << "Error en los parametros de entrada. Los parametros correctos son:" << std::endl;
-        std::cout << std::endl << "./main <activar podas>" << std::endl;
+        std::cout << std::endl << "./main <activar podas> <pasaje de datos>" << std::endl;
         std::cout << std::endl << "Activar podas: 1 para activarlas, 0 para desactivarlas." << std::endl;
         std::cout << "Si no se aclara, por defecto será 1." << std::endl;
+        std::cout << std::endl << "Pasaje de datos: 1 para que sea con archivos .in, 0 manual." << std::endl;
+        std::cout << "Por defecto se toma manual." << std::endl;
         return -1;
     }
 
-    int poda = 1;
-    if(argc == 3 && argv[2] == 0){poda = 0;}
+    bool poda = true;
+    if((argc == 3 || argc == 2) && argv[1] == "0"){poda = false;}
+    bool archivos == false;
+    if(argc == 3 && argv[2] == "1"){archivos = true;}
 
 
-    std::cout << std::endl << "Ingrese la cantidad de agentes:" << std::endl;
-    int cantidadAgentes = 0;
-    std::cin >> cantidadAgentes;
-    //creamos el vector de vectores de agentes, todo en cero
-    std::vector<std::vector<int > > agentes(cantidadAgentes);
-    for (int i = 0; i < cantidadAgentes; ++i){
-        for (int j = 0; j < cantidadAgentes; ++j){
-            agentes[i].push_back(0);
+    if(!archivos){
+        std::cout << std::endl << "Ingrese la cantidad de agentes:" << std::endl;
+        int cantidadAgentes = 0;
+        std::cin >> cantidadAgentes;
+        //creamos el vector de vectores de agentes, todo en cero
+        std::vector<std::vector<int > > agentes(cantidadAgentes);
+        for (int i = 0; i < cantidadAgentes; ++i){
+            for (int j = 0; j < cantidadAgentes; ++j){
+                agentes[i].push_back(0);
+            }
         }
+    
+        std::cout << std::endl << "Ingrese la cantidad de votos:" << std::endl;
+        int cantidadVotos;
+        std::cin >> cantidadVotos;
+    
+        std::cout << std::endl << "Ingrese todos los votantes con cada voto:" << std::endl;
+        for (int i = 0; i < cantidadVotos; ++i){
+            int votante;
+            std::cin >> votante;
+            int voto;
+            std::cin >> voto;
+            agentes[votante-1][modulo(voto)-1] = signo(voto);
+        }
+    } else {
+        //con .in
     }
 
-    std::cout << std::endl << "Ingrese la cantidad de votos:" << std::endl;
-    int cantidadVotos;
-    std::cin >> cantidadVotos;
 
-    std::cout << std::endl << "Ingrese todos los votantes con cada voto:" << std::endl;
-    for (int i = 0; i < cantidadVotos; ++i){
-        int votante;
-        std::cin >> votante;
-        int voto;
-        std::cin >> voto;
-        agentes[votante-1][modulo(voto)-1] = signo(voto);
-    }
-/*
-    std::cout << std::endl << cantidadAgentes << " " << cantidadVotos << std::endl;
-
-    for (int i = 0; i < cantidadAgentes; ++i){
-        imprimirVector(agentes[i]);
-    }
-*/
-
-
+    //creamos restantes y elegidos
     std::vector<int> restantes;
     for (int i = 0; i < cantidadAgentes; ++i){
         restantes.push_back(i);
     }
-
     std::vector<int> elegidos;
 
-    //confiablesConPodasAux(agentes, restantes, elegidos);
-    confiablesSinPodasAux(agentes, restantes, elegidos);
+    if(poda){confiablesConPodasAux(agentes, restantes, elegidos);}
+    else{confiablesSinPodasAux(agentes, restantes, elegidos);}
 
 }
