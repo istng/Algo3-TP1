fbd65ee7cb73874f77d4ee9262d0dbff1ee4c25d
istng <ivanstng@gmail.com>
2017-08-30 12:46:23 -0300

    va el corredor, no se si anda bien

--- corredor.sh
+++ corredor.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 for i in {1..100..1}
-  do 
-     python3 ./tests/corredorAgentes5.py > "./resultados/agentesVariados$i.csv"
-     python3 ./tests/corredorAgentes6.py > "./resultados/agentesVariados$i.csv"
-     python3 ./tests/corredorAgentes7.py > "./resultados/agentesVariados$i.csv"
-     python3 ./tests/corredorAgentes8.py > "./resultados/agentesVariados$i.csv"
-     python3 ./tests/corredorAgentes9.py > "./resultados/agentesVariados$i.csv"
-     python3 ./tests/corredorAgentes10.py > "./resultados/agentesVariados$i.csv"
-     python3 ./tests/corredorVotos.py > "./resultados/votosVariados$i.csv"
- done
+	do 
+		python3 ./tests/corredorAgentes5.py | ./agentes 0 #> "./resultados/agentesVariados$i.csv"
+		python3 ./tests/corredorAgentes5.py | ./agentes 1 #> "./resultados/agentesVariados$i.csv"
+		python3 ./tests/corredorAgentes6.py | ./agentes 0 #> "./resultados/agentesVariados$i.csv"
+		python3 ./tests/corredorAgentes6.py | ./agentes 1 #> "./resultados/agentesVariados$i.csv"
+		python3 ./tests/corredorAgentes7.py | ./agentes 0 #> "./resultados/agentesVariados$i.csv"
+		python3 ./tests/corredorAgentes7.py | ./agentes 1 #> "./resultados/agentesVariados$i.csv"
+		python3 ./tests/corredorAgentes8.py | ./agentes 0 #> "./resultados/agentesVariados$i.csv"
+		python3 ./tests/corredorAgentes8.py | ./agentes 1 #> "./resultados/agentesVariados$i.csv"
+		python3 ./tests/corredorAgentes9.py | ./agentes 0 #> "./resultados/agentesVariados$i.csv"
+		python3 ./tests/corredorAgentes9.py | ./agentes 1 #> "./resultados/agentesVariados$i.csv"
+		python3 ./tests/corredorAgentes10.py | ./agentes 0 #> "./resultados/agentesVariados$i.csv"
+		python3 ./tests/corredorAgentes10.py | ./agentes 1 #> "./resultados/agentesVariados$i.csv"
+		python3 ./tests/corredorVotos.py | ./agentes 0 #> "./resultados/votosVariados$i.csv"
+		python3 ./tests/corredorVotos.py | ./agentes 1 #> "./resultados/votosVariados$i.csv"
+done
