6e86986c2a85502b07fcef732003da405cfacada
istng <ivanstng@gmail.com>
2017-09-02 11:04:42 -0300

    corredor reiniciaba cada set de tests, arreglado

--- corredor.sh
+++ corredor.sh
@@ -1,63 +1,73 @@
 #!/bin/bash
 
-#for i in {1..100..1}
-#	do 
-#		echo variando agentes con podas en $i
-#		python3 ./tests/corredorAgentes.py | ./agentes 1 1 > "./resultados/agentesVariados_conPodas/$i.csv"
-#done
-#
-#for i in {1..100..1}
-#	do
-#		echo variando agentes sin podas en $i
-#		python3 ./tests/corredorAgentes.py | ./agentes 0 1 > "./resultados/agentesVariados_sinPodas/$i.csv"
-#done
+python3 ./tests/corredorAgentes.py > setAgentes
+python3 ./tests/corredorVotos.py > setVotos
+python3 ./tests/corredorCeros.py > setCeros
+
+for i in {1..100..1}
+	do 
+		echo variando agentes con podas en $i
+		./agentes 1 1 < setAgentes > "./resultados/agentesVariados_conPodas/$i.csv"
+done
+
+for i in {1..100..1}
+	do
+		echo variando agentes sin podas en $i
+		./agentes 0 1 < setAgentes > "./resultados/agentesVariados_sinPodas/$i.csv"
+done
 
 for i in {1..100..1}
 	do
 		echo variando votos en $i con podas
-		python3 ./tests/corredorVotos.py | ./agentes 1 1 > "./resultados/votosVariados_conPodas/$i.csv"
+		./agentes 1 1 < setVotos > "./resultados/votosVariados_conPodas/$i.csv"
 done
 
 
 for i in {1..100..1}
 	do
 		echo variando votos en $i con poda max
-		python3 ./tests/corredorVotos.py | ./agentes 2 1 > "./resultados/votosVariados_conPodaMax/$i.csv"
+		./agentes 2 1 < setVotos > "./resultados/votosVariados_conPodaMax/$i.csv"
 done
 
 
 for i in {1..100..1}
 	do
 		echo variando ceros en $i sin podas
-		python3 ./tests/corredorCeros.py | ./agentes 0 1 > "./resultados/cerosVariados_sinPodas/$i.csv"
+		./agentes 0 1 < setCeros > "./resultados/cerosVariados_sinPodas/$i.csv"
 done
 
 for i in {1..100..1}
 	do 
 		echo variando agentes en $i con poda max
-		python3 ./tests/corredorAgentes.py | ./agentes 2 1 > "./resultados/agentesVariados_conPodaMax/$i.csv"
+		./agentes 2 1 < setAgentes > "./resultados/agentesVariados_conPodaMax/$i.csv"
 done
 
 for i in {1..100..1}
 	do
 		echo variando agentes en $i con poda futuro
-		python3 ./tests/corredorAgentes.py | ./agentes 3 1 > "./resultados/agentesVariados_conPodaFuturo/$i.csv"
+		./agentes 3 1 < setAgentes > "./resultados/agentesVariados_conPodaFuturo/$i.csv"
 done
 
 for i in {1..100..1}
 	do
 		echo variando ceros en $i con podas
-		python3 ./tests/corredorCeros.py | ./agentes 1 1 > "./resultados/cerosVariados_conPodas/$i.csv"
+		./agentes 1 1 < setCeros > "./resultados/cerosVariados_conPodas/$i.csv"
 done
 
 for i in {1..100..1}
 	do
 		echo variando votos en $i con poda futuro
-		python3 ./tests/corredorVotos.py | ./agentes 3 1 > "./resultados/votosVariados_conPodaFuturo/$i.csv"
+		./agentes 3 1 < setVotos > "./resultados/votosVariados_conPodaFuturo/$i.csv"
 done
 
 for i in {1..100..1}
 	do
 		echo variando votos en $i sin podas
-		python3 ./tests/corredorVotos.py | ./agentes 0 1 > "./resultados/votosVariados_sinPodas/$i.csv"
+		./agentes 0 1 < setVotos > "./resultados/votosVariados_sinPodas/$i.csv"
+done
+
+for i in {1..100..1}
+	do 
+		echo variando ceros en $i con poda futuro
+		./agentes 3 1 < setCeros > "./resultados/cerosVariados_conPodaFuturo/$i.csv"
 done
